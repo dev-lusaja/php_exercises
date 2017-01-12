2a89d5bbcd0ef1888e2020e8659df7d0f0d84782
dev-lusaja <oscar_sanchez39@hotmail.com>
2017-01-11 20:59:22 -0500

    End example number 1.

--- ChangeString.php
+++ ChangeString.php
@@ -3,6 +3,8 @@
 class ChangeString
 {
 	private $alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+	private $is_upper;
+	private $is_final;
 
 	public function build($value)
 	{
@@ -10,33 +12,75 @@ class ChangeString
 		for ($i=0; $i < $len; $i++) {
 			$alphabet_value = $this->is_alphabet_value($value[$i]);
 			if ($alphabet_value) {
-				echo $alphabet_value;
+				$response = $alphabet_value;
 			} else {
-				echo $value[$i];
+				$response = $value[$i];
 			}
+			echo $response;
 		}
+		echo PHP_EOL;
 	}
 	
-	protected function is_alphabet_value($value)
+	protected function is_upper($value)
 	{
-		$is_upper = ctype_upper($value);
+		$this->is_upper = ctype_upper($value);
 		
-		if ($is_upper) {
+		if ($this->is_upper) {
 			$value = strtolower($value);
 		}
+		return $value;
+	}
+
+	protected function set_upper($value)
+	{
+		if ($this->is_upper) {
+			$value = strtoupper($value);
+		}
+		return $value;
+	}
+
+	protected function is_final($value)
+	{
+		$this->is_final = false;
+
+		if ($value == end($this->alphabet)) {
+			$this->is_final = true;
+			$value = $this->alphabet[0];
+		}
+		return $value;
+	}
+
+	protected function set_final($index)
+	{
+		$value = $this->alphabet[$index];
+		if ($this->is_final) {
+			$value = $this->alphabet[$index];
+		} else {
+			$value = $this->alphabet[$index + 1];
+		}
+		return $value;
+	}
+
+	protected function is_alphabet_value($value)
+	{
+		$value = $this->is_upper($value);
+		$value = $this->is_final($value);
 
 		$index = array_search($value, $this->alphabet);
+
 		if (!is_bool($index)) {
-			$response = $this->alphabet[$index + 1];
-			if ($is_upper) {
-				$response = strtoupper($this->alphabet[$index + 1]);
-			}
+			$value = $this->set_final($index);
+			$response = $this->set_upper($value);
 			return $response;
 		} else {
 			return false;
 		}
 	}
 }
+
+/**
+# execute the example with: php ChangeString.php "string"
+**/
 
 $changeString = new ChangeString();
 $changeString->build($argv[1]);
